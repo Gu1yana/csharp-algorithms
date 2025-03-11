43493cbf41f8aa6d83171d0561cf0d8c239e7407
Gu1yana <gulyanamk-bp110@code.edu.az>
2025-03-11 22:20:51 +0400

    task1

---
+++ TASK1/TASK1/Program.cs
@@ -0,0 +1,9 @@
+﻿string username = "Admin", password = "Admin123";
+if (username == "Admin" && password == "Admin123")
+{
+    Console.WriteLine("Xosh gelmisiniz!");
+}
+else
+{
+    Console.WriteLine("username ve ya password yanlishdir");
+}
